6b30866e8a4f546a53c933ec34057cb3575ba7ca
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:37:40 +0200

    feat: call the translate method on card_number with card_translation as an argument

--- main.py
+++ main.py
@@ -40,3 +40,14 @@ my_string = "tamperlot"
 translation_table = str.maketrans({'t': 'c', 'l': 'b'})
 translated_string = my_string.translate(translation_table)
 
+# Create a variable called translated_card_number and assign it the result of 
+# calling the translate method on card_number with card_translation as an argument.
+
+def main():
+    card_number = '4111-1111-4555-1142'
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+    print(translated_card_number)
+
+main()
+
