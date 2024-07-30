1e18f5ab10f78b0004708b185b447db53dd6876c
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:36:58 +0200

    feat: call translate method on my_string passing translated_string as an argument in translate

--- main.py
+++ main.py
@@ -31,4 +31,12 @@ def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-':'', ' ':''})
 
+# Step 4
+
+# Defining the translation does not in itself translate the string. 
+# The translate method must be called on the string to be translated with the translation table as an argument:
+
+my_string = "tamperlot"
+translation_table = str.maketrans({'t': 'c', 'l': 'b'})
+translated_string = my_string.translate(translation_table)
 
