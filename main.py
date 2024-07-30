b1747a0cb19f38a47ff07c70f20402bd8140ad60
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:40:47 +0200

    feat: Create a variable named card_number_reversed and assign it the value of the first 4 characters of card_number.

--- main.py
+++ main.py
@@ -88,3 +88,22 @@ def main():
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
 
+# Step 10
+
+# You have accessed elements (characters) of a string before, using the index operator []. 
+# You can also use the index operator to access a range of characters in a string with string[x:y:h]:
+
+my_string = 'camperbot'
+camper = my_string[0:6]
+cp = my_string[0:6:3]
+
+# Where x is the starting index, y is the ending index, and h is the step (the amount of characters to skip over).
+
+# Create a variable named card_number_reversed and assign it the value of the first 4 characters of card_number.
+
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[0:4][::-1]
+    print(card_number_reversed)
+    
+    
