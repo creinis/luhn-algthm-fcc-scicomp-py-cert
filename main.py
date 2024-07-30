0096b6e6ff2df1dccc27987b950fa87079dbeb10
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:35:20 +0200

    feat: Create a variable called card_translation and assign it a translation table to replace all - and characters with an empty string.

--- main.py
+++ main.py
@@ -23,4 +23,12 @@ def main():
 
 str.maketrans({'t': 'c', 'l': 'b'})
 
+# The above, when called on a string, will replace all t characters with c and all l characters with b.
+
+# Create a variable called card_translation and assign it a translation table to replace all - and characters with an empty string.
+
+def main():
+    card_number = '4111-1111-4555-1142'
+    card_translation = str.maketrans({'-':'', ' ':''})
+
 
