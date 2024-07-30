bd69e1e71258dec0acda131fafda521df9b6c23c
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:44:46 +0200

    feat: Below your print call, create a variable named sum_of_even_digits and assign it a value of 0.

--- main.py
+++ main.py
@@ -192,5 +192,19 @@ for digit in odd_digits:
     sum_of_odd_digits += int(digit)
     print(sum_of_odd_digits)
 
+# Step 22
+
+# Below your print call, create a variable named sum_of_even_digits and assign it a value of 0.
+
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
+    print(sum_of_odd_digits)
+    sum_of_even_digits = 0
+
 
 
