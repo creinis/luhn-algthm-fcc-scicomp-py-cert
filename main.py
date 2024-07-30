a1ab0114f6182118efb97fec0af3eecc1b1d3ef0
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:42:43 +0200

    feat: Within the verify_card_number function, create a variable odd_digits that contains every other digit of the card_number_reversed string.

--- main.py
+++ main.py
@@ -140,4 +140,13 @@ def verify_card_number(card_number):
     card_number_reversed = card_number[::-1]
     print(card_number_reversed)
 
+# Step 17
+
+# Within the verify_card_number function, create a variable odd_digits that contains every other digit of the card_number_reversed string.
+
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+
 
