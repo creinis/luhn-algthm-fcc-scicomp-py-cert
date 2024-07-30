20d3256190a9a83a698e388159f59cbfa8c3c0a4
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:33:07 +0200

    feat: declare variable inside function card_number with numeric value

--- main.py
+++ main.py
@@ -7,3 +7,11 @@
 
 def main():
     pass
+
+# Step 2
+
+# Replace the pass statement with a variable named card_number and a value of 4111-1111-4555-1142.
+
+def main():
+    card_number = "4111-1111-4555-1142"
+
