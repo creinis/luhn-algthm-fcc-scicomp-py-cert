d9eb45f4bf79a8228c557451046f1654c6bbd50d
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:41:46 +0200

    feat: Reverse the order of the digits in the last four digits of card_number, by using a slice with a step of -1. You can use either negative or positive indices for the start and end indices.

--- main.py
+++ main.py
@@ -115,4 +115,15 @@ def verify_card_number(card_number):
     card_number_reversed = card_number[0:4:2]
     print(card_number_reversed)
 
+# Step 13
+
+# Reverse the order of the digits in the last four digits of card_number, 
+# by using a slice with a step of -1. You can use either negative or positive indices 
+# for the start and end indices.
+
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[-1:-5:-1]
+    print(card_number_reversed)
+
 
