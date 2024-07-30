8a09088d57fc81b238fc26e75ce4869a6e5da004
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:38:15 +0200

    feat: define verify_card_number function

--- main.py
+++ main.py
@@ -51,3 +51,10 @@ def main():
 
 main()
 
+# Step 7
+
+# Define a function verify_card_number with a parameter card_number, and use the pass keyword to make the function do nothing.
+
+def verify_card_number(card_number):
+    pass
+
