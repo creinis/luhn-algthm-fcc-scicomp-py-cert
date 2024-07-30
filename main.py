79cd2391f7c69d2ed8096c6630273722330f328b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:43:23 +0200

    feat: Use a for loop to iterate over each digit in the odd_digits list. # Move your print call from the previous step into the for loop, and change it to print each digit

--- main.py
+++ main.py
@@ -149,4 +149,16 @@ def verify_card_number(card_number):
     card_number_reversed = card_number[::-1]
     odd_digits = card_number_reversed[::2]
 
+# Step 19
+
+# Use a for loop to iterate over each digit in the odd_digits list. 
+# Move your print call from the previous step into the for loop, and change it to print each digit.
+
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+    for digit in odd_digits:
+        print(digit)
+    
 
