976db9340ffa04f16798b62d822434ba9730bcb3
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 16:31:58 +0200

    feat: define main function

---
+++ main.py
@@ -0,0 +1,9 @@
+# Step 1
+
+#In this project, you will implement the Luhn Algorithm. 
+# This algorithm is a formula to validate a variety of identification numbers.
+
+# Start by declaring a function called main. Use the pass keyword to avoid an error.
+
+def main():
+    pass
